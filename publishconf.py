2a989176e6042392fc852ef1881dbf573a8c5432
Shauna Gordon-McKeon <shaunagm@gmail.com>
2025-05-01 13:15:29 -0400

    Back to https

--- publishconf.py
+++ publishconf.py
@@ -8,7 +8,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'http://shaunagm.github.io/static-site-tutorial'
+SITEURL = 'https://shaunagm.github.io/static-site-tutorial'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
